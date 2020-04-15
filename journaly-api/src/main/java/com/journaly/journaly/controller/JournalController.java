e62e74223a6fffbb06dd79a0ca466163a301ee26
Matheus Bernardes <bernardes.matheus@outlook.com>
2020-04-15 20:42:01 -0300

    Save and Get journal entries features

--- journaly-api/src/main/java/com/journaly/journaly/controller/JournalController.java
+++ journaly-api/src/main/java/com/journaly/journaly/controller/JournalController.java
@@ -1,18 +1,41 @@
 package com.journaly.journaly.controller;
 
 import com.journaly.journaly.model.JournalEntry;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import com.journaly.journaly.repository.JournalEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 public class JournalController {
 
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
+
     @CrossOrigin(origins = "http://localhost:3000")
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public @ResponseBody String saveJournalEntry(@RequestBody JournalEntry journalEntry) {
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public JournalEntry saveJournalEntry(@Valid @RequestBody JournalEntry journalEntry) {
         System.out.println(journalEntry.getContent());
-        return "Eu sou seu pai";
+        System.out.println(journalEntry.getDate());
+        return journalEntryRepository.save(journalEntry);
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/entries")
+    @ResponseStatus(HttpStatus.OK)
+    public List<JournalEntry> getJournalEntries() {
+        Comparator<JournalEntry> byDescendingDate = Collections.reverseOrder(Comparator.comparing(JournalEntry::getDate));
+        List<JournalEntry> journalEntryList = journalEntryRepository.findAll();
+        journalEntryList.sort(byDescendingDate);
+        return journalEntryList;
+    }
+
+
 
 }
